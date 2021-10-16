a32a47f485b48ac2c725ab08a0ac1aa0e2505377
lamatheus.g <m.goncalves@trustetc.com>
2021-10-15 21:50:09 -0300

    add vue and vue router to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+const { default: router } = require("./routes");
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+require("./bootstrap");
+
+window.Vue = require("vue").default;
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: "#app",
+    router
+});
---
+++ resources/js/routes.js
@@ -0,0 +1,16 @@
+import VueRouter from "vue-router";
+import ExampleComponent from "./components/ExampleComponent";
+
+const routes = [
+    {
+        path: "/",
+        component: ExampleComponent
+    }
+];
+
+const router = new VueRouter({
+    mode: "history",
+    routes
+});
+
+export default router;
